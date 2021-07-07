3136252ab7e9e2bd50fdb2e6b98e5b748b9948b6
Abel Katona <abelkatona@flywheel.io>
2021-07-07 14:19:35 +0200

    FLYW-8052 typo

--- qa_ci/ci/get_changelog_from_mr.sh
+++ qa_ci/ci/get_changelog_from_mr.sh
@@ -2,13 +2,14 @@
 test -z "$TRACE" || set -x
 set -euo pipefail
 USAGE="Usage:
-  $0
+  $0 CI_COMMIT_MESSAGE
 
 Get changelog from the MR's description.
 The MR ID is extracted from the commit message.
 "
 
 main() {
+    CI_COMMIT_MESSAGE=$1 && shift
     echo "$*" | grep -Eqvw -- "-h|--help|help" || { echo "$USAGE"; exit; }
     MR_ID_REGEX="!([0-9]+)"
     if [[ $CI_COMMIT_MESSAGE =~ $MR_ID_REGEX ]]
@@ -17,13 +18,15 @@ main() {
     else
         echo "Could not find MR ID in $CI_COMMIT_MESSAGE"; exit 1;
     fi
-    RESPONSE=$(curl -fLSs "$CI_API_V4_URL/projects/$CI_PROJECT_ID/merge_requests?iid[]=$MR_IID" \
+    echo "$MR_IID"
+
+    RESPONSE=$(curl -fLSs "$CI_API_V4_URL/projects/$CI_PROJECT_ID/merge_requests?iids[]=$MR_IID" \
         -H "Private-Token: $GITLAB_CI_BOT_TOKEN" \
         -H "Content-Type: application/json")
     MR_CNT=$(echo "$RESPONSE" | jq length)
     if [[ $MR_CNT != 1 ]]
     then
-        echo "MR was found with ID $MR_IID. Response: $RESPONSE"; exit 1;
+        echo "MR was not found with ID $MR_IID. Response: $RESPONSE"; exit 1;
     fi
     DESCRIPTION=$(echo "$RESPONSE" | jq -r '.[0].description')
     echo "$DESCRIPTION"
