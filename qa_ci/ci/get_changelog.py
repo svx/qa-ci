61585df1b569f2f9627176f27ae9f14860ebe67f
Abel Katona <abelkatona@flywheel.io>
2021-07-21 14:53:17 +0200

    FLYW-1234 test commit message

--- qa_ci/ci/get_changelog.py
+++ qa_ci/ci/get_changelog.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+
+import git
 
 def main():
-    parser = argparse.ArgumentParser(description=(
-        "Generate a changelog (markdown) from the merge commits found in a revision range.\n"
-        "By default, the range starts from the last tag and ends with the HEAD revision."
-    ))
+    parser = argparse.ArgumentParser(description="Generate a changelog (markdown) from the merge commits found in a revision range.")
     parser.add_argument('rev1', metavar='REV1', type=str, nargs='?', help='Start revision (default: last tag)')
     parser.add_argument('rev2', metavar='REV2', type=str, nargs='?', help='End revision (default: HEAD)')
 
     args = parser.parse_args()
+    rev1 = args.rev1
+    rev2 = args.rev2
+
+    repo = git.repo.Repo('./')
+    if not rev1:
+        try:
+            rev1 = repo.git.describe(abbrev=0, tags=True)
+        except git.exc.GitCommandError:
+            rev1 = ""
+        print(rev1)
+    if not rev2:
+        rev2 ="HEAD"
+        print(rev2)
+
+    revs = repo.git.log(f"{rev1}..{rev2}", merges=True, format="%h")
 
 
+    print(revs)
 
 
+#  REVS=$(git log --merges --format="%h" "$REV1..$REV2")
 
-
-
-
-
-
+# REV1=${1:-$(git describe --abbrev=0 --tags || true)}
 
 
 
